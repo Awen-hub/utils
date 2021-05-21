ccf3c04cbcfffedfda94dd8c7bb0bce8782d9071
liangyaowen.awen <liangyaowen.awen@bytedance.com>
2021-05-21 17:26:21 +0800

    fix(test): 修复+0和-0不相等的情况

--- test/date/getTimezone.spec.ts
+++ test/date/getTimezone.spec.ts
@@ -4,6 +4,5 @@ test("getTimezone", () => {
   const date: Date = new Date();
   const minutesDistance: number = -date.getTimezoneOffset();
   const { hours, minutes } = getTimezone();
-  console.log(hours, minutes, hours * 60 + minutes, minutesDistance);
-  expect(hours * 60 + minutes).toBe(minutesDistance);
+  expect(hours * 60 + minutes).toBeCloseTo(minutesDistance);
 });
