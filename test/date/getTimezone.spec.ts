9253f910c293d1d86aa395680e1b059d74f53f1b
liangyaowen.awen <liangyaowen.awen@bytedance.com>
2021-05-21 17:13:19 +0800

    test(action): 测试ci

--- test/date/getTimezone.spec.ts
+++ test/date/getTimezone.spec.ts
@@ -4,5 +4,6 @@ test("getTimezone", () => {
   const date: Date = new Date();
   const minutesDistance: number = -date.getTimezoneOffset();
   const { hours, minutes } = getTimezone();
+  console.log(hours, minutes, hours * 60 + minutes, minutesDistance);
   expect(hours * 60 + minutes).toBe(minutesDistance);
 });
