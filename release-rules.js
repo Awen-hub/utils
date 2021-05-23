3b126054cce9b44ecc123048a67fed663e7e7512
liangyaowen.awen <liangyaowen.awen@bytedance.com>
2021-05-23 12:45:00 +0800

    fix(发版): 发版
    ISSUES CLOSED: \

---
+++ release-rules.js
@@ -0,0 +1,14 @@
+module.exports = [{
+    type: "docs",
+    scope: "README",
+    release: "patch",
+  },
+  {
+    type: "refactor",
+    release: "minor",
+  },
+  {
+    type: "fix",
+    release: "patch",
+  },
+];
