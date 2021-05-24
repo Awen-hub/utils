c861178da47184d7970a959210f14c8d01a1f13f
liangyaowen.awen <liangyaowen.awen@bytedance.com>
2021-05-24 10:18:14 +0800

    config(global): 发版

--- scripts/patchVersion.js
+++
@@ -1,8 +0,0 @@
-const {
-  version
-} = require("../package.json");
-
-const versionList = version.split(".");
-versionList[2] = Number(versionList[2]) + 1;
-const newVersion = "v" + versionList.join(".");
-console.log(newVersion);
