dc83bc3ccfaf5ed1147ec26f5eca063926c92525
liangyaowen.awen <liangyaowen.awen@bytedance.com>
2021-05-24 14:28:57 +0800

    config(global): 发版

--- .releaserc.js
+++ .releaserc.js
@@ -4,7 +4,6 @@ module.exports = {
     [
       "@semantic-release/commit-analyzer", // 此处负责解析commit
       {
-        "preset": "angular",
         "config": "conventional-changelog-cmyr-config", //自定义配置，如果不填则是默认的conventional-changelog-angular
         "releaseRules": "./release-rules.js" //自定义配置，如果不填则是默认的conventional-changelog-angular
       }
